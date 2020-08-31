9bd4441d56bef3cfc272d6b66ecfce13779e4795
Shane McDonough <shanemcdman@gmail.com>
2020-08-31 16:19:35 -0700

    use post instead of multiple pages

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, redirect, request
 from markupsafe import escape
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD']= True
 
 on = False
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    global on
+    if request.method == 'POST':
+        if request.form['btn'] == 'on':
+            on = True
+        elif request.form['btn'] == 'off':
+            on = False
+        elif request.form['btn'] == 'toggle':
+            on = not on
     return render_template('index.html', on = on)
 
 @app.route('/test/<string:var>')
@@ -16,27 +24,6 @@ def test_world(var):
 @app.route('/template/<name>')
 def test_template(name):
     return render_template('name_template.html', name=name)
-
-@app.route('/on')
-def on():
-    global on
-    on = True
-    print("on:", on)
-    return index()
-
-@app.route('/off')
-def off():
-    global on
-    on = False
-    print("on:", on)
-    return index()
-
-@app.route('/toggle')
-def toggle():
-    global on
-    on = not on
-    print("on:", on)
-    return index()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
