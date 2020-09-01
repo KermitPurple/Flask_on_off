fe0ae8ce797f0555887cb2712df9813c30595f68
Shane McDonough <shanemcdman@gmail.com>
2020-09-01 06:22:11 +0100

    toggle light on raspi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for, render_template, redirect, request
 from markupsafe import escape
+import os
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD']= True
 
@@ -15,6 +16,11 @@ def index():
             on = False
         elif request.form['btn'] == 'toggle':
             on = not on
+        if on:
+            brightness = '1'
+        else:
+            brightness = '0'
+        os.system(f"echo {brightness} | sudo tee /sys/class/leds/led0/brightness")
     return render_template('index.html', on = on)
 
 @app.route('/test/<string:var>')
@@ -26,4 +32,4 @@ def test_template(name):
     return render_template('name_template.html', name=name)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
